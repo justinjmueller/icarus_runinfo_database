1c914b57d198fff4016a4e6153bb64bb652ca7dc
justinjmueller <justin.mueller@colostate.edu>
2023-12-20 10:10:04 -0600

    Made definition for a reduced trigger data table

---
+++ sql/tabledef_triggerdata_reduced.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS triggerdata (
+run_number integer NOT NULL,
+wr_seconds integer NOT NULL,
+wr_nanoseconds integer NOT NULL,
+gate_type integer NOT NULL,
+trigger_type integer NOT NULL
+);
